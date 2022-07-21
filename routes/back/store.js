1ab3ddc40e1177d74dd175d84b1aa6d7461e37a9
Eddie Lin <j951j951@gmail.com>
2022-07-21 14:43:02 +0800

    0721_eddie

--- routes/back/store.js
+++ routes/back/store.js
@@ -64,7 +64,7 @@ router.get('/apply/get', function(req, res){
 // 店家管理/店家/編輯/內頁/無檔案更新
 router.put('/edit', function(req, res){
     connect.query(
-        'update store set sto_name = ?, sto_tel = ?, sto_pay1 = ?, sto_pay2 = ?, sto_pay3 = ?, sto_pay4 = ?, sto_pay5 = ?, sto_pay6 = ?, sto_pay7 = ?, sto_thu = ?, sto_fri = ?, sto_sat = ?, sto_sun = ?, sto_fb = ?, sto_ins = ?, sto_line = ?, sto_info = ?, sto_sta = ?, where sto_id = ?',
+        'update store set sto_name = ?, sto_tel = ?, sto_pay1 = ?, sto_pay2 = ?, sto_pay3 = ?, sto_pay4 = ?, sto_pay5 = ?, sto_pay6 = ?, sto_pay7 = ?, sto_thu = ?, sto_fri = ?, sto_sat = ?, sto_sun = ?, sto_fb = ?, sto_ins = ?, sto_line = ?, sto_info = ?, sto_sta = ? where sto_id = ?',
         [req.body.name, req.body.tel, req.body.pay1, req.body.pay2, req.body.pay3, req.body.pay4, req.body.pay5, req.body.pay6, req.body.pay7, req.body.thu, req.body.fri, req.body.sat, req.body.sun, req.body.fb, req.body.ig, req.body.line, req.body.info, req.body.state, req.body.id],
         function (error, data) {
             if(error){
